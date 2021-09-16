beebe093447e0fb5975037aa07ff3202172317a8
yarikQA <ilienko030203@gmail.com>
2021-09-17 02:58:53 +0700

    Trying to make 1st test-case with search bar/ Now i'm catching error with 3rd url

--- test_yandex_search_bar.py
+++ test_yandex_search_bar.py
@@ -1,11 +1,10 @@
 import pytest
 from yandex_pages.yandex_page import YandexPage
 import keyboard  # для выполнения 5 шага первого тест-кейса
-import time
 
 
 class TestYandexSearchBar:
-    def test_tensor_is_on_find_results(self, browser):
+    def test_tensor_is_on_finded_results(self, browser):
         link = "https://yandex.ru/"
         page = YandexPage(browser, link)
         page.open()
@@ -13,4 +12,10 @@ class TestYandexSearchBar:
         page.input_to_searchbar("Тензор")
         page.user_should_see_suggest_list()
         keyboard.send("enter")  # имитация нажатия пользователем на Enter
-        time.sleep(5)
+
+        # проверка 5 url на наличие 'tensor.ru'
+        page.tensor_in_first_result_of_search()
+        page.tensor_in_second_result_of_search()
+        page.tensor_in_third_result_of_search()
+        page.tensor_in_fourth_result_of_search()
+        page.tensor_in_fifth_result_of_search()
