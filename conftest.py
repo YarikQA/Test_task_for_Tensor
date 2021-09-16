33c5ed382aea681cc7af996d60aceaeefd01eedf
yarikQA <ilienko030203@gmail.com>
2021-09-17 02:57:08 +0700

    Trying to add work with Firefox browser

--- conftest.py
+++ conftest.py
@@ -3,11 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+# Выбор браузера для работы
 def pytest_addoption(parser):
     parser.addoption("--browser_name", action='store', default='chrome',
                      help="Choose browser for work: Chrome of Firefox")
 
 
+# Фикстура для открытия браузера
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
@@ -19,11 +21,9 @@ def browser(request):
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\n\t Working with Chrome...")
-        browser = webdriver.Firefox(options=options)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("-- You should choose browser for work: Chrome or Firefox")
     yield browser
     print("\n\t Work is done...")
     browser.quit()
-
-
