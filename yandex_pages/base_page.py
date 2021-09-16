5772996859c084ac3fe2b01a49a272a3db4be397
yarikQA <ilienko030203@gmail.com>
2021-09-17 02:56:48 +0700

    Added method for find element on page with wait

--- yandex_pages/base_page.py
+++ yandex_pages/base_page.py
@@ -1,6 +1,9 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
+# Тут Базовые методы для работы с веб страницей, которые пригодятся в наследуемых классах
 class BasePage:
     def __init__(self, browser, url, timeout=5):
         self.browser = browser
@@ -14,5 +17,13 @@ class BasePage:
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
+            return False
+        return True
+
+    def is_element_on_page_with_wait(self, how, what, timeout=3):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            print("Can't find element with Explicit Waits")
             return False
         return True
