e83474e7bdca858c1c507f40f86723b77362fa57
yarikQA <ilienko030203@gmail.com>
2021-09-18 03:08:07 +0700

    RN doing second test_cast, now in the middle 5th step

--- test_yandex_search_bar.py
+++ test_yandex_search_bar.py
@@ -1,12 +1,15 @@
 import pytest
-from yandex_pages.yandex_page import YandexPage
+from yandex_pages.yandex_page import YandexSearchPage
+from yandex_pages.yandex_page import YandexImagesPage
 import keyboard  # для выполнения 5 шага первого тест-кейса
+import time
 
 
+@pytest.mark.skip
 class TestYandexSearchBar:
     def test_tensor_is_on_finded_results(self, browser):
         link = "https://yandex.ru/"
-        page = YandexPage(browser, link)
+        page = YandexSearchPage(browser, link)
         page.open()
         page.user_should_see_search_bar()
         page.input_to_searchbar("Тензор")
@@ -19,3 +22,19 @@ class TestYandexSearchBar:
         page.tensor_in_third_result_of_search()
         page.tensor_in_fourth_result_of_search()
         page.tensor_in_fifth_result_of_search()
+
+
+@pytest.mark.images
+class TestYandexImages:
+    def test_yandex_images(self, browser):
+        link = "https://yandex.ru/"
+        page = YandexImagesPage(browser, link)
+        page.open()
+        page.user_should_see_images()
+        page.click_on_the_images_link()
+        time.sleep(3)
+        page.current_url_is_yandex_images()
+        page.click_on_the_first_image()
+        page.current_page_is_first_image()
+
+
