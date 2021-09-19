be6a7f2b83e71cc57dd945f5195011913f49a46e
yarikQA <ilienko030203@gmail.com>
2021-09-20 05:29:06 +0700

    idk why, but firefox don't wanna work good! Deleted choosing browser

--- conftest.py
+++ conftest.py
@@ -6,24 +6,20 @@ from selenium.webdriver.chrome.options import Options
 # Выбор браузера для работы
 def pytest_addoption(parser):
     parser.addoption("--browser_name", action='store', default='chrome',
-                     help="Choose browser for work: Chrome of Firefox")
+                     help="Choose chrome browser for work")
 
 
 # Фикстура для открытия браузера
 @pytest.fixture(scope="function")
 def browser(request):
     options = Options()
-    fp = webdriver.FirefoxProfile
     browser_name = request.config.getoption("browser_name")
     browser = None
     if browser_name == "chrome":
         print("\n\t Working with Chrome...")
         browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\n\t Working with Chrome...")
-        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("-- You should choose browser for work: Chrome or Firefox")
+        raise pytest.UsageError("-- You should choose chrome browser for work")
     yield browser
     print("\n\t Work is done...")
     browser.quit()
