39c7d874b76687d1adea653f1dbc8585dcf206b4
yarikQA <ilienko030203@gmail.com>
2021-09-20 05:35:15 +0700

    Did second test-case, but need to some reworks

--- test_yandex_search_and_images.py
+++ test_yandex_search_and_images.py
@@ -1,12 +1,13 @@
 import pytest
-from yandex_pages.yandex_page import YandexSearchPage
-from yandex_pages.yandex_page import YandexImagesPage
+from yandex_pages.yandex_search_page import YandexSearchPage
+from yandex_pages.yandex_images_page import YandexImagesPage
 import keyboard  # для выполнения 5 шага первого тест-кейса
 import time
 
 
-@pytest.mark.skip
-class TestYandexSearchBar:
+@pytest.mark.search
+@pytest.mark.xfail
+class TestYandexSearchBar:  # Тесты поисковой строки
     def test_tensor_is_on_finded_results(self, browser):
         link = "https://yandex.ru/"
         page = YandexSearchPage(browser, link)
@@ -16,7 +17,7 @@ class TestYandexSearchBar:
         page.user_should_see_suggest_list()
         keyboard.send("enter")  # имитация нажатия пользователем на Enter
 
-        # проверка 5 url на наличие 'tensor.ru'
+        # проверка 5 url(результатов поиска) на наличие 'tensor.ru'
         page.tensor_in_first_result_of_search()
         page.tensor_in_second_result_of_search()
         page.tensor_in_third_result_of_search()
@@ -25,16 +26,19 @@ class TestYandexSearchBar:
 
 
 @pytest.mark.images
-class TestYandexImages:
+class TestYandexImages:  # Тесты страницы Яндекс.Картинки
     def test_yandex_images(self, browser):
         link = "https://yandex.ru/"
         page = YandexImagesPage(browser, link)
         page.open()
-        page.user_should_see_images()
+        page.user_should_see_images_icon()
         page.click_on_the_images_link()
-        time.sleep(3)
         page.current_url_is_yandex_images()
-        page.click_on_the_first_image()
-        page.current_page_is_first_image()
 
+        page.click_on_the_first_category()
+        page.current_page_is_not_yandex_images()  # Если открыта НЕ базовая страница, значит открылся url 1 категории
+        page.get_text_of_category_in_search_bar()
+        page.name_of_category_is_text_in_search_bar()
+        page.click_on_first_image()
+        page.picture_is_changing_after_click_forward_and_behind()
 
